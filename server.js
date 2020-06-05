af8efbd592c446710cdb6a77a2b67f41a372e5fa
Daniel <Djohnsen143@gmail.com>
2020-06-05 18:35:09 +0200

    more heroku

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
+const path = require('path');
+
 const express = require("express");
 const app = express();
 
-app.use(express.static(__dirname + "/dist"));
+app.use(express.static(__dirname + "/dist/wishlist"));
 
-app.all("*", (req, res) =>{
-   res.status(200).sendFile(__dirname + "/dist/index.html")
-})
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname + '/dist/wishlist/index.html'));
+    console.log("server server server server")
+});
 
 app.listen(process.env.PORT || 8000);
